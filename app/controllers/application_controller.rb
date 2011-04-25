819570ea121c81a683360cd138adff0f581b9a08
yortz <skylarking420@gmail.com>
2011-04-25 17:20:25 +0200

    configured devise default sign in path, configured dj admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # def after_sign_in_path_for(resource_or_scope)
+  #   if resource_or_scope.is_a?(current_user)
+  #     current_user_pictures_url(current_user)
+  #   else
+  #     super
+  #   end
+  # end
+  
+  def delayed_job_admin_authentication
+    # authentication_logic_goes_here
+  end
 end
